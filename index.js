e5ffe238e06e0fcf67f0d678359d360890fca9ce
keyla <keylacaucota0@gmail.com>
2025-02-11 11:22:57 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+// Se tilizará un script de Node.js simple para explorar algunas características básicas de Prisma Client.
+const { PrismaClient } = require('@prisma/client')
+
+const prisma = new PrismaClient()
+
+async function main() {
+    // ... you will write your Prisma Client queries here
+    const allUsers = await prisma.user.findMany()
+    console.log(allUsers)
+  }
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
+
+
+
